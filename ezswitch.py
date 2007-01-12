e8cbf1758fe0af59ef5342e1a1c8564ef0e1bdac
Marius Gedminas <marius@gedmin.as>
2007-01-12 19:27:53 +0000

    Make ezswitch.py -l list all vailable branches.

--- ezswitch.py
+++ ezswitch.py
@@ -22,6 +22,14 @@ def svninfo(path):
     return stdout.read()
 
 
+def svnls(url):
+    """
+    Return a list of files under ``url``.
+    """
+    stdout, stdin = popen2.popen2('svn ls %s' % url)
+    return stdout.read()
+
+
 def currentbranch(path, svninfo=svninfo):
     """
     Let's set up a dummy 'svn info' command handler:
@@ -60,6 +68,7 @@ def currentbranch(path, svninfo=svninfo):
     else:
         url = lines[2][5:]
     return url
+
 
 def determinebranch(branch, path, svninfo=svninfo):
     """
@@ -126,16 +135,71 @@ def determinebranch(branch, path, svninfo=svninfo):
     return '/'.join(new_chunks)
 
 
+def listbranches(path, svninfo=svninfo, svnls=svnls):
+    r"""
+    Let's set up a dummy 'svn info' command handler:
+
+      >>> def dummyinfo(path):
+      ...     return '''\
+      ... Path: .
+      ... URL: http://dev.worldcookery.com/svn/bla/trunk/blergh
+      ... Repository UUID: ab69c8a2-bfcb-0310-9bff-acb20127a769
+      ... Revision: 1654
+      ... Node Kind: directory
+      ... '''
+
+    and a dummy 'svn ls' as well:
+
+      >>> def dummyls(path):
+      ...     assert path == 'http://dev.worldcookery.com/svn/bla/branches'
+      ...     return '''\
+      ... foo/
+      ... README.txt
+      ... bar/
+      ... baz/
+      ... '''
+
+    ``listbranches()`` takes the svn path of the current working
+    directory path, finds the URL of the repository, and lists all branches
+    in that repository.
+
+      >>> listbranches('.', svninfo=dummyinfo, svnls=dummyls)
+      ['foo', 'bar', 'baz']
+ 
+    """
+    url = currentbranch(path, svninfo=svninfo)
+    chunks = url.split('/')
+
+    while chunks:
+        ch = chunks.pop()
+        if ch in ('branch', 'branches'):
+            chunks.append(ch)
+            break
+        elif ch == 'trunk':
+            chunks.append('branches')
+            break
+
+    branches = []
+    for line in svnls('/'.join(chunks)).splitlines():
+        if line.endswith('/'):
+            branches.append(line[:-1])
+    return branches
+
+
 def main(argv):
     progname = os.path.basename(argv[0])
     parser = optparse.OptionParser(
-                "usage: %prog [options] branch [wc-path]\n"
+                "usage: %prog [-n] [-c] branch [wc-path]\n"
+                "       %prog -l\n"
                 "       %prog",
                 prog=progname,
                 description="Switch the working directory to a different"
                             " Subversion branch.  When run without"
                             " arguments, %prog will print the"
                             " URL of the current branch.")
+    parser.add_option('-l', '--list',
+                      help='list existing branches',
+                      action='store_true', dest='list_branches', default=False)
     parser.add_option('-c', '--create-branch',
                       help='create the new branch before switching to it',
                       action='store_true', dest='create_branch', default=False)
@@ -149,13 +213,17 @@ def main(argv):
     except optparse.OptParseError, e:
         sys.exit(e)
 
+    path = '.'
+
+    if opts.list_branches:
+        print '\n'.join(listbranches(path))
+        return
+
     if not args:
-        path = '.'
         print currentbranch(path)
         return
 
     branch = args[0]
-    path = '.'
     if len(args) > 1:
         path = args[1]
 
